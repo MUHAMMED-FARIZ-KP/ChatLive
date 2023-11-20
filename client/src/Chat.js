8a511d7fd4446991ed2fa476add988b7a6020c05
Muhammed Fariz KP <farizz7676off@gmail.com>
2023-11-20 16:42:10 +0530

    css againn uodated

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -27,7 +27,7 @@ function Chat({socket,username,room }) {
     return (
     <div className='chat-window'>
         <div className='chat-header'>
-            <p>Live Chat</p>
+            <p id="room-p">Room-id:  {room}</p>
         </div>
         <div className='chat-body'>
             <ScrollToBottom className='message-container'>
