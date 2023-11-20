f30ef319aa9b70919782ffac1bbb1cd9bf0d1065
Muhammed Fariz KP <farizz7676off@gmail.com>
2023-11-20 16:02:28 +0530

    make again local host

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import Chat from './Chat';
 import io from 'socket.io-client'
 
-// const socket=io.connect("http://localhost:3001")
-const socket=io.connect("https://raspy-potent-fender.glitch.me")
+const socket=io.connect("http://localhost:3001")
 
 function App() {
   const [username,setUsername]=useState("")
