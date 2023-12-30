892998436fa2645a163192ee09f6950988501f4f
Muhammed Fariz KP <farizz7676off@gmail.com>
2023-12-30 10:31:14 +0530

    updated

--- Server/index.js
+++ Server/index.js
@@ -8,7 +8,8 @@ app.use(cors());
 const server=http.createServer(app);
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        
+        origin:"https://chat-live-react.vercel.app/",
         methods:["GET","POST"],
     },
 });
